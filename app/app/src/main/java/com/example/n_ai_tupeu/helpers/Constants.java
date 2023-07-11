59d0ca99ddb09b779fb0860103265ac269e43129
AndreeaMusca <andreeamusca382@gmail.com>
2023-07-11 12:02:43 +0300

    Fix something

--- app/app/src/main/java/com/example/n_ai_tupeu/helpers/Constants.java
+++ app/app/src/main/java/com/example/n_ai_tupeu/helpers/Constants.java
@@ -2,6 +2,7 @@ package com.example.n_ai_tupeu.helpers;
 
 public class Constants {
     public static final String BASE_URL = "http://192.168.39.82:5132";
+    //"http://192.168.120.60:5132";
     public static final String ACCOUNTS_ENDPOINT = "/Accounts";
     public static final String GET_ENDPOINT ="/Accounts/getByUsernameAndPassword";
 
